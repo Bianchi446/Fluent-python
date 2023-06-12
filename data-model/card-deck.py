272b2cc5807ca7f5d2eea32072fa56a3f18eda21
Bianchi446 <josebianchi221js@gmail.com>
2023-06-12 06:12:57 -0300

    Pythonic card deck

--- data-model/card-deck.py
+++ data-model/card-deck.py
@@ -1,55 +1,31 @@
-# Example : The __len__ and __getitem__  special methods
+# implementing two special classes : __getitem__ and  __len__
 
 
-import collections 
+import collections
+from random import choice
 
-Card = collections.namedtuple('Card', ['rank', 'suit'])
- 
-class FrenchDeck:
-    ranks = [str(n) for n in range(2, 11 )] + list('JQKA')
+Card =  collections.namedtuple('Card', ['Rank', 'Suit'])
+
+class FrenchDeck :
+    ranks = [str(n) for n in range(2,11)] + list('JQKA')
     suits = 'spades diamonds clubs hearts'.split()
 
     def __init__(self):
         self._cards = [Card(rank, suit) for suit in self.suits
-                                          for rank in self.ranks]
+                                        for rank in self.ranks]
 
     def __len__(self):
         return len(self._cards)
 
     def __getitem__(self, position):
-        return self._cards[position] 
+        return self._cards[position]
 
 
-beer_card = Card("7", "diamond")
+beer_card = Card('6', 'Diamond')
 print(beer_card)
 
-    # checking the lenght of FrenchDeck class
+deck = FrenchDeck()
 
-deck = FrenchDeck()    
-len(deck)
+print(len(deck))
 
-    # Example 2 : Making a random choice of cards 
-
-from random import choice 
-
-choice(deck)
-
-    # Example 3 : Sort the ranking cards 
-
-suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
-
-def spades_high(card):
-    rank_value = FrenchDeck.ranks.index(card.rank)
-    return rank_value + len(suit_values) + suit_values[card.suit]
-
-for card in sorted(deck, key=spades_high):
-    print(card)
-
-
-
-# More uses of special methods 
-
-# 1. emulating numeric types
-# 2. String representation of objects
-# 3. Boolean value of an object
-# 4. Implementing collections
+print(choice(deck))
